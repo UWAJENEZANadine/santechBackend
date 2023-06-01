948082e971dc0a8b9523d914f578e1274ad08b17
UWAJENEZANadine <adelineu640@gmail.com>
2023-06-01 17:04:30 +0200

    login

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const app = express();
-const mysql = require("mysql");
-const cors = require("cors")
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-app.use(cors())
-app.use(express.json())
+
+const app = express();
 
 const db = mysql.createConnection({
   user: "root",
@@ -13,37 +13,74 @@ const db = mysql.createConnection({
   database: "santech",
 });
 
-app.post("/create", (req, res) => {
-  const name = req.body.name;
-  const age = req.body.age;
-  const country = req.body.country;
-  const position = req.body.position;
-  const wage = req.body.wage;
-
-  db.query(
-    "INSERT INTO user (name, age, country, position, wage) VALUES (?,?,?,?,?)",
-    [name, age, country, position, wage],
-    (err, result) => {
-      if (err){
-        console.log(err)
-      } else{
-        res.send("Data inserted successfully")
-      }
-    }
-  );
-});
-
-
-app.get('/users', (req, res)=>{
-  db.query("SELECT * FROM user", (err, result)=>{
-    if(err){
-      console.log(err)
-    }else{
-      res.send(result)
-    }
-  })
+db.connect((error)=>{
+  if(error) throw error;
+  console.log("database connected successfully")
 })
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+app.post('/login', (req, res)=>{
+
+  const { username = "info@santechrwanda.com", password ="santechrwanda21"} = req.body;
+  const sql = "SELECT * FROM loginn WHERE username = ? AND password = ? ";
+
+  db.query(sql, [username, password], (err, results) =>{
+    if (err) {
+      console.error('Error executing MySQL query');
+      throw err;
+    }
+
+    if (results.length > 0) {
+      res.json({ success: true, message: 'Login successful' });
+    } else {
+      res.json({ Error: false, message: 'Invalid username or password' });
+    }
+  });
+});
 
 app.listen(4040, () => {
   console.log("server is running on port 4040");
 });
+
+
+
+
+// app.post("/create", (req, res) => {
+//   const name = req.body.name;
+//   const age = req.body.age;
+//   const country = req.body.country;
+//   const position = req.body.position;
+//   const wage = req.body.wage;
+
+//   db.query(
+//     "INSERT INTO user (name, age, country, position, wage) VALUES (?,?,?,?,?)",
+//     [name, age, country, position, wage],
+//     (err, result) => {
+//       if (err){
+//         console.log(err)
+//       } else{
+//         res.send("Data inserted successfully")
+//       }
+//     }
+//   ); 
+// });
+
+
+// app.get('/users', (req, res)=>{
+//   db.query("SELECT * FROM user", (err, result)=>{
+//     if(err){
+//       console.log(err)
+//     }else{
+//       res.send(result)
+//     }
+//   })
+// })
+
+
+
